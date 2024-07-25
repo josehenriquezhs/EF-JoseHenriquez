bc6993eacbbe95c25c5a20e1d6de1ab9b152d5d9
jose henriquez <henriquez.jsehs@gmail.com>
2024-07-25 20:22:26 -0300

    prueba 2

--- data/stock.js
+++ data/stock.js
@@ -3,30 +3,30 @@ const productos =
     {
         id: 1,
         nombre: "Jordan air 1 OG",
-        tipo: "buzo",
-        bio: "Un buzo que re va con vos",
+        tipo: "Zapatillas 1",
+        bio: "Jordan air 1 OG quedan pocas",
         precio: 158000,
-        imagen: './public/images/jordan_2.webp',
+        imagen: '/public/images/jordan_2.webp',
         cantidad:1
     },
 
     {
         id: 2,
         nombre: "Jordan 11 OG",
-        tipo: "remera",
-        bio: "Una remera que re va con vos",
+        tipo: "Zpatillas 2",
+        bio: "Jordan 11 Og del momento",
         precio: 160000,
-        imagen: './public/images/jordan_3.webp',
+        imagen: '/public/images/jordan_3.webp',
         cantidad:1
     },
 
     {
         id: 3,
         nombre: "Jordan 4 Retro",
-        tipo: "camisa",
-        bio: "Una camisa que re va con vos",
+        tipo: "Zapatillas 3",
+        bio: "Jordan 4 Retro chetas para le verano",
         precio: 150000,
-        imagen: './public/images/jordan_4.webp',
+        imagen: '/public/images/jordan_4.webp',
         cantidad:1
     },
     
