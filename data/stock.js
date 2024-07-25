6020d997daac4f9960c191aebbe7feb045a2f70f
jose henriquez <henriquez.jsehs@gmail.com>
2024-07-25 20:34:18 -0300

    final

--- data/stock.js
+++ data/stock.js
@@ -3,31 +3,31 @@ const productos =
     {
         id: 1,
         nombre: "Jordan air 1 OG",
-        tipo: "Zapatillas 1",
+        tipo: "Zapatillas 0",
         bio: "Jordan air 1 OG quedan pocas",
-        precio: 158000,
+        precio: 170000,
         imagen: '/public/images/jordan_2.webp',
-        cantidad:1
+        cantidad: 1
     },
 
     {
         id: 2,
         nombre: "Jordan 11 OG",
-        tipo: "Zpatillas 2",
+        tipo: "Zpatillas 1",
         bio: "Jordan 11 Og del momento",
         precio: 160000,
         imagen: '/public/images/jordan_3.webp',
-        cantidad:1
+        cantidad: 1
     },
 
     {
         id: 3,
         nombre: "Jordan 4 Retro",
-        tipo: "Zapatillas 3",
+        tipo: "Zapatillas 2",
         bio: "Jordan 4 Retro chetas para le verano",
         precio: 150000,
         imagen: '/public/images/jordan_4.webp',
-        cantidad:1
+        cantidad: 1
     },
-    
+
 ];
